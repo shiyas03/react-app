5a7303f613d2032d34c181ce72cc8eaae39ffdbb
shiyas03 <shiyastofficial@gmail.com>
2024-10-12 12:28:26 +0530

    Use React Context API to pass Data through component tree without using Props Data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 // import Button from './components/button/button'
 // import HelloWorld from './components/hello-world/hello-world'
 import Posts from './components/posts/posts'
+import SideBar from './components/sideBar/sideBar';
+import ButtonContext from './context/buttonContext';
 
 function App() {
   return (
-    <div className="">
+    <div className="flex px-5">
 
-      <Posts />
+      <div className="w-1/4">
+        <ButtonContext.Provider value="side button context" >
+          <SideBar />
+        </ButtonContext.Provider>
+      </div>
+      <div className="w-3/4">
+        <Posts />
+      </div>
 
       {/* <Button name="First Button"/>
       <Button name="Second Button"/>
---
+++ src/components/sideBar/sideBar.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Navigation from "../navigation/navigation";
+
+export default class SideBar extends Component{
+    render(){
+        return (
+            <div>
+                <Navigation/>
+            </div>
+        )
+    }
+}
